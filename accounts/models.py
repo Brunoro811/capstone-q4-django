70b00201f07cb96a35b9ee09b8f8846105600fd1
Bruno <ferreira.bruno.oliveira@gmail.com>
2022-05-23 13:59:00 -0300

    feat: create AccountsModel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+
+
+class AccountsModel(AbstractUser):
+    
+    id = models.UUIDField(primary_key=True,default=uuid4,editable=False)
+    email = models.EmailField(unique=True)
+    username= models.CharField(unique=False,null=True,max_length=150)
+    last_name = models.CharField(max_length=255)
+    is_seller = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now,editable=False)
+
+
+
+    USERNAME_FIELD = 'email' 
+    REQUIRED_FIELDS: list[str] = []
