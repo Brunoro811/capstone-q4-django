54e734c5c5fac2ba43879b68c09e56fce265a2d8
Bruno <ferreira.bruno.oliveira@gmail.com>
2022-05-24 09:21:27 -0300

    feat: create paths and files for productModel

---
+++ products/tests/test_model_product.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+class ProductModelTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls) -> None:
+        return super().setUpTestData()  
+    
+    def test_model_products_fields(self):
+        ...
+        
---
+++ products/tests/utils/value_product.py
@@ -0,0 +1,11 @@
+
+list_products = [
+    {
+        'name': 'Camiseta Casual',
+        'cost_value': 15.99,
+        'sale_value_retail': 46.99,
+        'sale_value_wholesale': 35.99,
+        'quantity_wholesale': 10,
+        #'link': 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTf4aCzsWneZEtVmitSJwry-SFHr0AjvMRWDZ-CtRQRTkjoa6k1jtMrknBBmJMsF1oQ_nO10ef6tT0&usqp=CAc'
+    }
+]
