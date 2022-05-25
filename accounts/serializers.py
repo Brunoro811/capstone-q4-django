cf4d9307a95e29df2aeacb9fdd930867f10febb9
Etnan Sousa <poketnans@gmail.com>
2022-05-25 20:58:08 -0300

    feat: retrieve and update one serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 
+from accounts.models import AccountModel
+
 
 class LoginSerializer(serializers.Serializer):
-    
+
     username = serializers.CharField()
     password = serializers.CharField()
+
+
+class RetrieveUpdateOneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountModel
+        fields = "__all__"
