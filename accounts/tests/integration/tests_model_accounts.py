8a29c9b30b8f5dc7634a186a435a3c4dd19a225e
pedromenimen <77471145+pedromenimen@users.noreply.github.com>
2022-05-25 16:21:27 -0300

    fix: changed route names

--- accounts/tests/integration/tests_model_accounts.py
+++ accounts/tests/integration/tests_model_accounts.py
@@ -13,14 +13,14 @@ class TestAccounst(APITestCase):
         cls.seller_token = Token.objects.get_or_create(user=test_seller)[0].key
 
     def test_if_cant_list_all_without_being_logged(self):
-        response = self.client.get("/api/accounts/", format="json")
+        response = self.client.get("/accounts/", format="json")
 
         self.assertEqual(response.status_code, 401)
         self.assertIn("detail", response.json())
 
     def test_if_can_list_all_users_as_admin(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.admin_token)
-        response = self.client.get("/api/accounts/", format="json")
+        response = self.client.get("/accounts/", format="json")
 
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), list)
@@ -28,7 +28,7 @@ class TestAccounst(APITestCase):
     def test_if_cant_list_all_users_as_seller(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.seller_token)
 
-        response = self.client.get("/api/accounts/", format="json")
+        response = self.client.get("/accounts/", format="json")
 
         self.assertEqual(response.status_code, 403)
         self.assertIn("detail", response.json())
