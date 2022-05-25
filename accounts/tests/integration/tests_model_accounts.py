a1c656679e5a77543f9f0ec8540750b25218ebed
Pedro <pedromenimen@gmail.com>
2022-05-24 21:33:34 -0300

    fix: fixed error that should appear later

--- accounts/tests/integration/tests_model_accounts.py
+++ accounts/tests/integration/tests_model_accounts.py
@@ -1,5 +1,7 @@
 from operator import itemgetter
 
+from accounts.models import AccountModel
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
 from ..utils import user_admin_correct, user_seller_correct
@@ -8,40 +10,46 @@ from ..utils import user_admin_correct, user_seller_correct
 class TestAccounst(APITestCase):
     @classmethod
     def setUpTestData(cls):
-        email, password = itemgetter('email', 'password')(user_admin_correct)
-        admin_login_info = {'email': email, 'password': password}
-        email, password = itemgetter('email', 'password')(user_seller_correct)
-        seller_login_info = {'email': email, 'password': password}
+        email, password = itemgetter("email", "password")(user_admin_correct)
+        admin_login_info = {"email": email, "password": password}
+        email, password = itemgetter("email", "password")(user_seller_correct)
+        seller_login_info = {"email": email, "password": password}
+        superuser = AccountModel.objects.create_superuser(
+            username="Pedraum", email="pedro@gmail.com"
+        )
+        cls.superuser_token = Token.objects.get_or_create(superuser)
         cls.admin_login_info = admin_login_info
         cls.seller_login_info = seller_login_info
         cls.admin_register_info = user_admin_correct
         cls.seller_register_info = user_seller_correct
 
     def test_cant_list_all_without_being_logged(self):
-        response = self.client.get('/api/accounts/', format='json')
+        response = self.client.get("/api/accounts/", format="json")
 
         self.assertEqual(response.status_code, 401)
-        self.assertIn('detail', response.json())
+        self.assertIn("detail", response.json())
 
     def test_can_list_all_users_as_admin(self):
-        self.client.post('/api/accounts/', self.admin_register_info, format='json')
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.superuser_token)
+        self.client.post("/api/accounts/", self.admin_register_info, format="json")
         token = self.client.post(
-            '/api/login/', self.admin_login_info, format='json'
-        ).json()['token']
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-        response = self.client.get('/api/accounts/', format='json')
+            "/api/login/", self.admin_login_info, format="json"
+        ).json()["token"]
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token)
+        response = self.client.get("/api/accounts/", format="json")
 
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), list)
 
     def test_cant_list_all_users_as_seller(self):
-        self.client.post('/api/accounts/', self.seller_register_info, format='json')
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.superuser_token)
+        self.client.post("/api/accounts/", self.seller_register_info, format="json")
         token = self.client.post(
-            '/api/login/', self.admin_login_info, format='json'
-        ).json()['token']
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+            "/api/login/", self.admin_login_info, format="json"
+        ).json()["token"]
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token)
 
-        response = self.client.get('/api/accounts/', format='json')
+        response = self.client.get("/api/accounts/", format="json")
 
         self.assertEqual(response.status_code, 403)
-        self.assertIn('detail', response.json())
+        self.assertIn("detail", response.json())
