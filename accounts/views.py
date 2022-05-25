2c6b6cf147a151889897fb22e32784dd1abc49a6
Bruno <ferreira.bruno.oliveira@gmail.com>
2022-05-25 10:30:26 -0300

    feat: create initial model view update accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateAPIView
 
-# Create your views here.
+
+class AccountsRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    ...
+    
