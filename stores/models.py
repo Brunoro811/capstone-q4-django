a71fc56d0c227e12b565cb354cb238acc97254bc
Bruno <ferreira.bruno.oliveira@gmail.com>
2022-05-25 10:33:10 -0300

    feat: add unique name True in table store

--- stores/models.py
+++ stores/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class StoreModel(models.Model):
     
     id = models.UUIDField(primary_key=True,default=uuid4,editable=False)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,unique=True)
     street = models.CharField(max_length=255)
     number = models.IntegerField()
     zip_code = models.CharField(max_length=9)
