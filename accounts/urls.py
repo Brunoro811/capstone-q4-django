41a65ffeed0838e8395e6a8a3197356e5fd05e60
Etnan Sousa <poketnans@gmail.com>
2022-05-25 21:00:09 -0300

    feat: retrieve and update one url pattern

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from accounts.views import login
+from accounts.views import RetrieveUpdateOneView, login
 
 urlpatterns = [
-    path('login/', login),
+    path("login/", login),
+    path("accounts/<user_id>/", RetrieveUpdateOneView.as_view()),
 ]
