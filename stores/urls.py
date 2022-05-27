15c451ce3259a0c89962eb206a995da3bfaa7445
juliagamaol <gamaj235@gmail.com>
2022-05-27 20:17:06 -0300

    feat: add url stores by store_id

--- stores/urls.py
+++ stores/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from stores.views import StoreByIdView
+from stores.views import ListCreateStores, StoreByIdView
 
 urlpatterns = [
+    path("stores/", ListCreateStores.as_view()),
     path('stores/<store_id>', StoreByIdView.as_view()),
 ]
