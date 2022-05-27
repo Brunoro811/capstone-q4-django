4665b97d155a7a74e1dcf6302b5b48339a01094b
juliagamaol <gamaj235@gmail.com>
2022-05-27 20:18:08 -0300

    feat: add view get store by store_id

--- stores/views.py
+++ stores/views.py
@@ -1,16 +1,43 @@
+import django
+from rest_framework import generics, status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveUpdateAPIView
 
+from stores.exception import StoreNameAlreadyExists
 from stores.models import StoreModel
 from stores.permissions import IsAdmin
-from stores.serializers import StoreModelSerializer
+from stores.serializers import StoreModelByIdSerializer, StoreModelSerializer
 
 
-class StoreByIdView(RetrieveAPIView):
+class ListCreateStores(generics.ListCreateAPIView):
+    permission_classes = [IsAdmin]
+    authentication_classes = [TokenAuthentication]
+    queryset = StoreModel.objects.all()
+    serializer_class = StoreModelSerializer
+
+    def post(self, request, *args, **kwargs):
+        """
+        Create a store
+        """
+        store = (
+            StoreModel.objects.filter(name=self.request.data["name"]).exists()
+        )
+        if store:
+            raise StoreNameAlreadyExists
+        return super().post(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        """
+        Get list of all stores
+        """
+        return super().get(request, *args, **kwargs)
+
+class StoreByIdView(RetrieveUpdateAPIView):
 
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAdmin]
 
     queryset = StoreModel.objects.all()
-    serializer_class = StoreModelSerializer
+    serializer_class = StoreModelByIdSerializer
     lookup_url_kwarg = "store_id"
+    
