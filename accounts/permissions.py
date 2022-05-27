192e4b6a8ff01bd55336afa4b754ae7c190ace51
Bruno <ferreira.bruno.oliveira@gmail.com>
2022-05-27 09:52:48 -0300

    feat: create new permission for is admin

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -5,7 +5,19 @@ from rest_framework.request import Request
 class IsAuthenticatedAccounts(IsAuthenticated):
     
     def has_permission(self, request, view):
-        restrict_methods = ('GET','PATCH',)
+        restrict_methods = ('GET','POST','PATCH',)
         if request.method in restrict_methods:
             return super().has_permission(request, view)
+        return True
+
+
+class IsAdmin(BasePermission):
+    
+    def has_permission(self, request, _):
+        restrict_methods = ('POST',)
+        if request.method in restrict_methods and (
+            not request.user.is_admin
+        ):
+            return False
+
         return True
