d5085327ac0d75e4fd6488478777fdb5c17a1fa0
juliagamaol <gamaj235@gmail.com>
2022-05-27 20:23:12 -0300

    test: add get store by store_id respose fields

--- stores/tests/utils/__init__.py
+++ stores/tests/utils/__init__.py
@@ -1,2 +1,3 @@
 from stores.tests.utils.values_store import (
-    get_store_by_id_200_response_fields, store_correct)
+    fields_request_create_store, fields_response_create_store,
+    get_store_by_id_200_response_fields, store_correct, store_success)
--- stores/tests/utils/values_store.py
+++ stores/tests/utils/values_store.py
@@ -6,13 +6,13 @@ fake = Faker()
 
 
 store_correct = {
-    'name': fake.company(),
-    'street': fake.street_name(),
-    'number': fake.building_number(),
-    'state': fake.current_country_code(),
-    'other_information': '',
-    'zip_code': fake.postcode(),
-    'updated_at': datetime.utcnow()
+    "name": fake.company(),
+    "street": fake.street_name(),
+    "number": fake.building_number(),
+    "state": fake.current_country_code(),
+    "other_information": "",
+    "zip_code": fake.postcode(),
+    "updated_at": datetime.utcnow(),
 }
 
 get_store_by_id_200_response_fields = [
@@ -28,4 +28,35 @@ get_store_by_id_200_response_fields = [
     "updated_at",
     "sellers",
     "admins"
+]
+store_success = {
+    "name": "Empresa limitada",
+    "street": "rua tal",
+    "number": 555,
+    "state": "MG",
+    "other_information": "Nã é fantasma",
+    "zip_code": "99999999",
+}
+
+fields_response_create_store = [
+    "id",
+    "name",
+    "state",
+    "street",
+    "number",
+    "zip_code",
+    "is_active",
+    "other_information",
+    "created_at",
+    "updated_at",
+]
+
+
+fields_request_create_store = [
+    "name",
+    "street",
+    "number",
+    "state",
+    "other_information",
+    "zip_code",
 ]
