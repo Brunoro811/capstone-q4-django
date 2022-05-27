4da417c7f6b2c2b38fd418d8ec0508d139f0d823
juliagamaol <gamaj235@gmail.com>
2022-05-26 23:28:47 -0300

    test: route GET stores by store_id

--- stores/tests/utils/__init__.py
+++ stores/tests/utils/__init__.py
@@ -1,1 +1,2 @@
-from stores.tests.utils.values_store import store_correct
+from stores.tests.utils.values_store import (
+    get_store_by_id_200_response_fields, store_correct)
--- stores/tests/utils/values_store.py
+++ stores/tests/utils/values_store.py
@@ -15,3 +15,17 @@ store_correct = {
     'updated_at': datetime.utcnow()
 }
 
+get_store_by_id_200_response_fields = [
+    "id",
+    "name",
+    "state",
+    "street",
+    "number",
+    "zip_code",
+    "is_active",
+    "other_information",
+    "created_at",
+    "updated_at",
+    "sellers",
+    "admins"
+]
