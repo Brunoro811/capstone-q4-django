d76a03a15dc85dee79a6dc8ff3870f47cc0bf8af
juliagamaol <gamaj235@gmail.com>
2022-05-28 10:22:52 -0300

    feat: add serializer to get stores by store id

--- stores/serializers.py
+++ stores/serializers.py
@@ -1,3 +1,5 @@
+from accounts.models import AccountModel
+from accounts.serializers import AccountSerializer
 from django.utils import timezone
 from rest_framework import serializers
 
@@ -34,3 +36,30 @@ class StoreModelSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data["updated_at"] = timezone.now()
         return super().create(validated_data)
+
+class StoreModelByIdSerializer(serializers.ModelSerializer):   
+    class Meta:
+        model = StoreModel
+        fields = [
+            "id",
+            "name",
+            "state",
+            "street",
+            "number",
+            "zip_code",
+            "is_active",
+            "other_information",
+            "created_at",
+            "updated_at"
+        ]
+        read_only_fields = ["id", "is_active", "created_at", "updated_at"]
+
+        def to_representation(self, instance: StoreModel):
+            sellers_to_store = AccountModel.objects.filter(is_seller=True)
+            admins_to_store = AccountModel.objects.filter(is_admin=True)
+            
+            ret = super().to_representation(instance)
+            ret["sellers"] = AccountSerializer(sellers_to_store, many=True).data
+            ret["admins"] = AccountSerializer(admins_to_store, many=True).data
+
+            return ret
