fe0ef66f3076b948ed6cb10c70e2b166b53f302b
Bruno <ferreira.bruno.oliveira@gmail.com>
2022-05-28 09:21:41 -0300

    feat: create route update

--- stores/serializers.py
+++ stores/serializers.py
@@ -34,3 +34,31 @@ class StoreModelSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data["updated_at"] = timezone.now()
         return super().create(validated_data)
+
+class StoreModelUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StoreModel
+        fields = [
+            "id",
+            "name",
+            "state",
+            "street",
+            "number",
+            "zip_code",
+            "other_information",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["id", "is_active", "created_at", "updated_at"]
+
+        extra_kwargs = {
+            "name":{"help_text": "Unique. Field is string"},
+            "state":{"help_text": "Field is string"},
+            "street":{"help_text": "Field is string"},
+            "number":{"help_text": "Field is int"},
+            "zip_code":{"help_text": "Field is string. Max 10 caracters"}, 
+            "is_active":{"help_text": "Field is boolean"},
+            "other_information":{"help_text": "Field is string. Max 150 caracters"},           
+        }
+
+    
