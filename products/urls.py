5a25f36f6fe3c8fe8254588f143863a6f3b852ab
pedromenimen <77471145+pedromenimen@users.noreply.github.com>
2022-05-30 15:17:04 -0300

    fix: url fixed

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from products import views
 from django.urls import path
 
 urlpatterns = [
-    path("product/<product_id>/", views.GetUpdateProductView.as_view()),
+    path("products/<product_id>/", views.GetUpdateProductView.as_view()),
 ]
