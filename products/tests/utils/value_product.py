698c39f6de5fa84a73a5e6693a27587ab2c92bd1
Bruno <ferreira.bruno.oliveira@gmail.com>
2022-05-30 14:26:57 -0300

    test: create deafault product shirt

--- products/tests/utils/value_product.py
+++ products/tests/utils/value_product.py
@@ -1,11 +1,19 @@
+from categorys.models import CategoryModel
+from stores.models import StoreModel
 
-list_products = [
-    {
+
+def product_shirt ():
+
+    """
+        This function return dict shirt with `category_id`  and `store_id` empty.
+    """
+
+    return {
         'name': 'Camiseta Casual',
         'cost_value': 15.99,
         'sale_value_retail': 46.99,
         'sale_value_wholesale': 35.99,
+        'store_id': StoreModel.objects.all()[0] ,
+        'category_id': StoreModel.objects.all()[0],
         'quantity_wholesale': 10,
-        #'link': 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTf4aCzsWneZEtVmitSJwry-SFHr0AjvMRWDZ-CtRQRTkjoa6k1jtMrknBBmJMsF1oQ_nO10ef6tT0&usqp=CAc'
     }
-]
