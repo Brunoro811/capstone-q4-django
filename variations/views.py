c1d526cb6b3307a3b9a90d7a2e6562661c7fa6b8
Etnan Sousa <poketnans@gmail.com>
2022-05-31 17:00:37 -0300

    feat: adds permission classes in view

--- variations/views.py
+++ variations/views.py
@@ -2,11 +2,18 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import ListCreateAPIView
 
 from variations.models import VariationModel
-from variations.serializers import listUpdateSerializer
+from variations.permissions import (
+    ListCreateAuthenticatePermission,
+    ListCreateAuthorizePermission,
+)
+from variations.serializers import ListUpdateSerializer
 
 
 class ListCreateProductVariationView(ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = []
+    permission_classes = [
+        ListCreateAuthenticatePermission,
+        ListCreateAuthorizePermission,
+    ]
     queryset = VariationModel.objects.all()
-    serializer_class = listUpdateSerializer
+    serializer_class = ListUpdateSerializer
