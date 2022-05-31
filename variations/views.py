13818181968573ae06b782f4e7be2a23f735ac64
Etnan Sousa <poketnans@gmail.com>
2022-05-31 13:29:29 -0300

    feat: list update products variations view

--- variations/views.py
+++ variations/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from variations.models import VariationModel
+from variations.serializers import listUpdateSerializer
+
+
+class ListCreateProductVariationView(ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = []
+    queryset = VariationModel.objects.all()
+    serializer_class = listUpdateSerializer
