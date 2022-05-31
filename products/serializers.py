396f6d5cfcd7b69c02b86056a8d86429719af279
Pedro <pedromenimen@gmail.com>
2022-05-31 10:51:50 -0300

    fix: now its working perfectly

--- products/serializers.py
+++ products/serializers.py
@@ -1,3 +1,4 @@
+from categorys.models import CategoryModel
 from rest_framework import serializers
 
 from products.models import ProductModel
@@ -6,4 +7,27 @@ from products.models import ProductModel
 class GetUpdateProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductModel
-        fields = "__all__"
+        fields = [
+            "id",
+            "name",
+            "cost_value",
+            "sale_value_retail",
+            "sale_value_wholesale",
+            "quantity_wholesale",
+            "is_active",
+            "store_id",
+            "category_id",
+            # "category",
+        ]
+        extra_kwargs = {"category_id": {"write_only": True}}
+
+    def to_internal_value(self, data):
+        if "category" in data.keys():
+            category = CategoryModel.objects.get_or_create(name=data.get("category"))[0]
+            self.instance.category_id = category
+        return super().to_internal_value(data)
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret["category"] = instance.category_id.name
+        return ret
